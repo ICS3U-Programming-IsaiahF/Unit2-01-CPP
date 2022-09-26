94490c27ac19df24b554d1b9e56eaab506046b55
Isaiah Fernandez <isaiah.fernandez@stu.ocsb.ca>
2022-09-26 01:15:13 +0000

    Added areaPerCirc.cpp

---
+++ areaPerCirc.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Isaiah fernandez All rights reserved
+// .
+// Created by: Isaiah Fernandez
+// Date: September 24, 2022
+// This program calculates the perimeter and area of a circle
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "If a circle has a radius of " << std::endl;
+    std::cout << "6cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (M_PI*pow(6, 2)) << "cm^2" << std::endl;
+    std::cout << "Perimeter is " << 2 * M_PI * 6 << "cm" << std::endl;
+}
